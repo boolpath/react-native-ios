8dbe8a70a9755df31493b64a2b2cf68e27cfc6b3
Jorge Zaccaro <jorgezaccaro@gmail.com>
2016-04-06 19:42:44 -0500

    First commit

---
+++ index.ios.js
@@ -0,0 +1,27 @@
+/**
+ * React Native iOS Native UI Components
+ */
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class ios extends Component {
+  render() {
+    return (
+      <View>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+AppRegistry.registerComponent('ios', () => ios);
