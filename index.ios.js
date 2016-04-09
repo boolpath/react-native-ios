941fcde3face72c75a8958ebc1f64af5a0509419
Jorge Zaccaro <jorgezaccaro@gmail.com>
2016-04-09 14:08:43 -0500

    Segmented Control iOS

--- index.ios.js
+++ index.ios.js
@@ -7,21 +7,45 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  SegmentedControlIOS
 } from 'react-native';
 
 class ios extends Component {
   render() {
     return (
-      <View>
-        
+      <View style={styles.container}>
+        <View style={styles.controlView}>
+          <SegmentedControlIOS values={['One', 'Two']} />
+        </View>
+        <View style={styles.controlView}>
+          <SegmentedControlIOS values={['One', 'Two', 'Three']} selectedIndex={0} />
+        </View>
+        <View style={styles.controlView}>
+          <SegmentedControlIOS values={['One', 'Two', 'Three', 'Four', 'Five']} selectedIndex={4} />
+        </View>
+        <View style={styles.controlView}>
+          <SegmentedControlIOS values={['One', 'Two']} momentary={true} />
+        </View>
+        <View style={styles.controlView}>
+          <SegmentedControlIOS values={['One', 'Two']} enabled={false} />
+        </View>
+        <View style={styles.controlView}>
+          <SegmentedControlIOS values={['One', 'Two']} tintColor="#aaa" />
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    paddingTop: 64
+  },
+  controlView: {
+    margin: 10,
+    marginBottom: 25
+  }
 });
 
 AppRegistry.registerComponent('ios', () => ios);
